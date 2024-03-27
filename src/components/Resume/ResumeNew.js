290265273353a244f2b801c1b804714880d68d77
Saurabh <saurabhjaybhaye01@gmail.com>
2024-03-27 20:09:21 +0530

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -32,8 +32,22 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document file={pdf}>
+            <Page
+              pageNumber={1}
+              className="d-flex justify-content-center"
+              scale={width > 786 ? 1.7 : 0.6}
+            />
+            <Page
+              pageNumber={2}
+              className="d-flex justify-content-center"
+              scale={width > 786 ? 1.7 : 0.6}
+            />
+            <Page
+              pageNumber={3}
+              className="d-flex justify-content-center"
+              scale={width > 786 ? 1.7 : 0.6}
+            />
           </Document>
         </Row>
 
